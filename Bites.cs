2bf75fdb993d62e2cf3ac3b2a48f4b80100b6c07
Дмитрий Матохин <argizol@mail.ru>
2023-10-16 20:55:17 +0300

    Реализованы неявные приведения из byte, long, int к Bits

---
+++ Bites.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSpecGB
+{
+    internal class Bits
+    {
+        byte Value { get; set; }
+        public Bits(byte bits) { this.Value = bits}
+        public static implicit operator byte(Bits bits) => bits.Value;
+        public static implicit operator long(Bits bits) => bits.Value;
+        public static implicit operator int(Bits bits) => bits.Value;
+        public static explicit operator Bits(byte bits) => new Bits(bits);
+
+    }
+}
+
