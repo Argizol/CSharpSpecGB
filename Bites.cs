7368546b3aadb1b3c5d1bc2a08492f8eb3db553e
Дмитрий Матохин <argizol@mail.ru>
2023-10-16 23:11:16 +0300

    Revert "Исправлено ДЗ2 "Реализуйте операторы неявного приведения из long,int,byt в Bits.""
    This reverts commit 83b7d0acfada8002001b8e708085d7fb170df13d.

--- Bites.cs
+++ Bites.cs
@@ -3,17 +3,24 @@
     //Реализуйте операторы неявного приведения из long,int,byt в Bits.
     internal class Bits
     {
-        byte Value { get; set; }        
+        byte Value { get; set; }
         public Bits(byte bits) { Value = bits; }
-        public static implicit operator Bits(byte bits) => new Bits(bits);
-        public static implicit operator Bits(long bits)
+        public static implicit operator byte(Bits bits)
         {
             return new Bits(Convert.ToByte(bits));
         }
-        public static implicit operator Bits(int bits)
+        public static implicit operator long(Bits bits)
         {
-            return new Bits(Convert.ToByte(bits));
+            return new Bits(Convert.ToByte((bits)));
         }
+        public static implicit operator int(Bits bits)
+        {
+            return new Bits(Convert.ToByte((bits)));
+        }
+        public static explicit operator Bits(byte bits) => new Bits(bits);
+
     }
+
+
 }
 
