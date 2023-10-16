0d9ec5659205d18e8056c9dbc2797c3697253dd8
Дмитрий Матохин <argizol@mail.ru>
2023-10-16 23:08:25 +0300

    ДЗ2

---
+++ IndexException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSpecGB
+{
+    public class IndexException : Exception
+    {
+        public IndexException(string messge) : base(messge) { }
+    }
+}
