8cf8e3e2a82d4a6f8377160cc1603b518eca30ac
Дмитрий Матохин <argizol@mail.ru>
2023-10-23 23:22:32 +0300

    Реализовано домашнее задание 4

---
+++ Program.cs
@@ -0,0 +1,10 @@
+//Дан массив и число. Найдите три числа в массиве сумма которых равна искомому числу.
+
+using CSharpSpecGB.Seminar4;
+
+var numsArr = new int[] { 1, 13, 7, 21, 1, 3, 5, 6, 8, 9 };
+
+
+HW4.FindSumParts(numsArr, 21);
+
+
---
+++ Seminar4/HW4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpSpecGB.Seminar4
+{
+    internal static class HW4
+    {
+        public static bool FindSumParts(int[] numsArr, int findNum)
+        {        
+            for (int i = 1; i < numsArr.Length - 1; i++)
+            {
+                int left = i - 1;
+                int right = i + 1;
+                int result;
+
+                result = numsArr[left] + numsArr[i] + numsArr[right];
+                if (result == findNum)
+                {
+                    Console.WriteLine($"Искомое число {findNum} состоит из {numsArr[left]} + {numsArr[i]} + {numsArr[right]}");
+                    return true;
+                }
+                else
+                {
+                    left++;
+                    right++;
+                }                
+            }
+            Console.WriteLine("Подходящих чисел не найдено");
+            return false;
+        }
+    }
+}
