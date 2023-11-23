6e021f66d3b60550b3b048174729d52cf295f84f
Дмитрий Матохин <argizol@mail.ru>
2023-11-23 11:00:13 +0300

    Добавлено решение для 7 ДЗ

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using C_2._0;
 using CSharpSpecGB.Seminar5;
 using System;
 using System.Collections.Generic;
@@ -7,39 +8,9 @@ class Program()
 {
     static void Main(string[] args)
     {
-        Calc calc = new();
-        calc.handler += SubscribeMyEvent;
-        string exitCommand = String.Empty;
-        string operation = String.Empty;
-        string command = String.Empty;        
-
-        Console.WriteLine("Добро пожаловать в странный калькулятор по странному заданию");
-        Console.WriteLine();
-
-        while (true)
-        {     
-            calc.ExecuteOperation();
-
-            Console.WriteLine("Продолжить работу?");
-            Console.Write("Для завершения работы введите:" +
-                " Отмена или поставьте пробел и нажмите Enter\n" +
-                "Для продолжения работы нажмите Enter");
-
-            exitCommand = Console.ReadLine();
-
-            if (exitCommand.ToLower().Equals("отмена") || exitCommand.ToLower().Equals(" "))
-            {
-                Console.WriteLine("Для завершения работы нажмите любую кнопу");
-                Console.ReadKey(true);
-                break;
-            }
-        }
-    }
-    public static void SubscribeMyEvent(object? sender, EventArgs e)
-    {
-        if (sender is Calc)
-            Console.WriteLine(((Calc)sender)._result);
-    }
+        Reflection reflection = new();
+        reflection.Run(args);
+    }   
 }
 
 
