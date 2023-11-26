317b8cd8dc1697781bd80e3978919b0760e6668c
Дмитрий Матохин <argizol@mail.ru>
2023-11-26 12:45:37 +0300

    Преобразуем JSON в XML

--- Program.cs
+++ Program.cs
@@ -1,13 +1,42 @@
+using Newtonsoft.Json;
 using Seminar7;
+using System.Diagnostics;
+using System.IO;
+using System.Xml.Linq;
 
 class Program()
 {
+    [STAThread]
     static void Main(string[] args)
     {
-        Reflection reflection = new();
-        reflection.Run(args);
-    }   
 
+        //ћожно через боль и JsonDocument, но зачем? Ѕерем нормальную бибилиотеку и делаем по человечески:
+
+        string path = string.Empty;
+        using (OpenFileDialog openFileDialog = new OpenFileDialog())
+        {
+            openFileDialog.InitialDirectory = @"Seminar9";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                path = Path.Combine(openFileDialog.ToString());
+                
+            }
+        }
+
+        using (StreamReader sr = new StreamReader(path)) 
+        {
+            sr.ReadToEnd();
+            XNode? node = JsonConvert.DeserializeXNode(sr.ReadToEnd());
+            Console.WriteLine(node?.ToString());
+        }
+        
+        
+    } 
 }
 
 
